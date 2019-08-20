0535a42bd0d826d573e0052f90180e8a10d3ba26
Elina <elinagoldin@gmail.com>
2019-08-20 14:52:50 -0700

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,19 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "../components/section-banner.scss"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section id="not-found" className="banner not-found">
+      <div className="wrapper">
+        <h1 className="banner__title">NOT FOUND</h1>
+        <h2 className="banner__subtitle">
+          This page doesn&#39;t exist. Oh the disappointment!
+        </h2>
+      </div>
+    </section>
   </Layout>
 )
 
